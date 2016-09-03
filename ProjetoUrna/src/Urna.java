29b4f0e06fce8091645e70683a59fe572f3a291c
Paulo Gustavo <paulogusstavo@Paulos-MacBook-Pro.local>
2016-09-03 16:00:16 -0300

    VersãoInicial

---
+++ ProjetoUrna/src/Urna.java
@@ -0,0 +1,15 @@
+
+public class Urna {
+	private Candidato candidato;
+	
+	public boolean votar(int numero) {
+		
+		return true;
+	}
+	
+	private boolean confirmacao() {
+		
+		return true;
+	}
+
+}
